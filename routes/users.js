7026610be871c3644c27ce341da64a94d3e0f853
singleAlones <m13319191328@163.com>
2019-12-13 17:13:32 +0800

    reg

--- routes/users.js
+++ routes/users.js
@@ -3,48 +3,26 @@ var router = express.Router();
 var pool=require('../pool');
 /* GET users listing. */
 router.get('/user', function(req, res, next) {
-   var $phone =req.query.phone;
-   var str='select * from h_user where hphone=?';
-   pool.query(str,[$phone],function(err,result){
-     if(err) throw err;
-     console.log(result)
-     if(result.length>0){
-       res.send({code:0,msg:'hased phone'})
-     }else{
        var obj=req.query;
-       var str='insert into h_user set ?';
-       pool.query(str,[obj],function(err,result){
-         if(err) throw err;
-         if(result.affectedRows>0){
-           res.send({code:200,msg:'signin succ'})
-         }else{
-           res.send({code:301,msg:'signin fail'})
-         }
-       }) 
-     }
-   })
+       console.log(obj);
+       if(obj.hname==""||obj.hphone==""){
+           res.send({code:400,msg:'用户名或密码为空'})
+       }else{
+        var reg= /^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/;
+          if(reg.test(obj.hphone)&&obj.hname!=""){
+            var str='insert into h_user set ?';
+            pool.query(str,[obj],function(err,result){
+              if(err) throw err;
+              if(result.affectedRows>0){
+                res.send({code:200,msg:'预留信息成功'})
+              }else{
+                res.send({code:301,msg:'预留信息失败，请重新输入'})
+              }
+            }) 
+          }else{
+            res.send({code:305,msg:'手机号格式不正确，请重新输入'})
+          }
+       }   
 });
-router.get('/update',function(req,res){
-  var obj=req.query;
-  var sql="update h_user set hname= ? ,hphone= ? ,hgender= ?,haddress= ?  where hid= ? ";
-  pool.query(sql,[obj.hname,obj.hphone,obj.hgender,obj.haddress,obj.hid],function(err,result){
-    if(err) throw err;
-    if(result.affectedRows>0){
-      res.send({code:200,msg:"update succ"})
-    }else{
-      res.send({code:301,msg:'update fail'})
-    }
-  })
-})
-//注册
-router.get('/reg',function(req,res,next){
-  var hname=req.query.uname;
-  var hphone=req.query.uphone;
-  console.log(hname,hphone)
-  var sql=`insert into h_user values hname= ? and hphone=?`;
-  pool.query(sql,[hname,hphone],(err,result)=>{
-   if(err) throw(err);
-    res.send(JSON.stringify(result));
- })
-});
+ 
 module.exports = router;  
